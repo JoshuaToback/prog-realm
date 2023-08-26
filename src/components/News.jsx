32dfc282804c30f3fb9b2a1acdfc07112f55fc20
JoshuaToback <joshuatoback@gmail.com>
2023-08-26 14:51:05 -0400

    Added Upcoming Sets and blog posts

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 
 function News() {
+  const posts = [
+    {
+      id: 1,
+      title: "Site Launch",
+      date: '08/31/2023',
+      content:
+        "Welcome to the Site! We're really excited to have you here and follow along our card games!",
+    },
+
+    // Add more posts here...
+  ];
+
   return (
-    <div className="news-container">
-      <div className="news-entry">
-        <h2 className="news-title">News</h2>
-        <h3>Welcome to the Site!</h3>
-        <p className="news-date">08/14/2023</p>
-        <p>This is a test!</p>
+    <div className="blog">
+      <h1>Progression Realm News</h1>
+      <div className="posts">
+        {posts.map((post) => (
+          <div key={post.id} className="post">
+            <h2>{post.title}</h2>
+            <h3>{post.date}</h3>
+            <p>{post.content}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/components/Standings.jsx
+++ src/components/Standings.jsx
@@ -3,47 +3,96 @@ import React from "react";
 function Standings() {
   return (
     <div class="standings-container">
-    <div class="standings-wrapper">
-      <h2 class="standings-title">Standings</h2>
-      <table class="standings-table">
-        <thead>
-          <tr>
-            <th>Duelists</th>
-            <th>Stats</th>
-            <th>Sets Won</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Violet</td>
-            <td>106 - 66 - 1</td>
-            <td>17</td>
-          </tr>
-          <tr>
-            <td>Jordan</td>
-            <td>81 - 47 - 1</td>
-            <td>10</td>
-          </tr>
-          <tr>
-            <td>Pop</td>
-            <td>53 - 63</td>
-            <td>5</td>
-          </tr>
-          <tr>
-            <td>Josh</td>
-            <td>51 - 80</td>
-            <td>3</td>
-          </tr>
-          <tr>
-            <td>Luis</td>
-            <td>30 - 69</td>
-            <td>2</td>
-          </tr>
-        </tbody>
-      </table>
+      <div class="standings-wrapper">
+        <h2 class="standings-title">Standings</h2>
+        <table class="standings-table">
+          <thead>
+            <tr>
+              <th>Duelists</th>
+              <th>Stats</th>
+              <th>Sets Won</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Violet</td>
+              <td>106 - 66 - 1</td>
+              <td>17</td>
+            </tr>
+            <tr>
+              <td>Jordan</td>
+              <td>81 - 47 - 1</td>
+              <td>10</td>
+            </tr>
+            <tr>
+              <td>Pop</td>
+              <td>53 - 63</td>
+              <td>5</td>
+            </tr>
+            <tr>
+              <td>Josh</td>
+              <td>51 - 80</td>
+              <td>3</td>
+            </tr>
+            <tr>
+              <td>Luis</td>
+              <td>30 - 69</td>
+              <td>2</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="upcoming-sets">
+        <h2>Upcoming Sets</h2>
+        <ul>
+          <li>
+            <a
+              href="https://ygoprodeck.com/pack/The%20Duelist%20Genesis"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              The Duelist Genesis
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://ygoprodeck.com/pack/Crossroads%20of%20Chaos"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Crossroads of Chaos
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://ygoprodeck.com/pack/Dark%20Legends"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Dark Legends
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://ygoprodeck.com/pack/Duelist%20Pack%3A%20Yusei"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Duelist Pack 8: Yusei
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://ygoprodeck.com/pack/Crimson%20Crisis"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Crimson Crisis
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-  
   );
 }
 
